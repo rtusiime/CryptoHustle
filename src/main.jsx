7d6668947210a99c077834b3141d8585a9a8ce56
Kevin Tusiime <rtusiime@u.rochester.edu>
2024-04-05 21:21:08 -0400

    checkpoint 3

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './routes/Layout.jsx'
+import DetailView from './components/CoinDetail.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index={true} element={<App />} />
+          <Route index={false} path='/coinDetails/:symbol' element={<DetailView/>} />
           </Route>
       </Routes>
     </BrowserRouter>
